625eb66ae90397b7400e53e421727c7fa3641172
yadriel.rivera <yadriel.rivera@upr.edu>
2026-03-08 12:15:35 -0300

    Row 3 update
    row 3 is drawn successfully

--- src/laberinto.s
+++ src/laberinto.s
@@ -1,8 +1,16 @@
 .data
 
 startLED: .word 0xf0000090
-rowPathLen: .word 16, 12, 12, 12, 20, 12, 24, -1, 4,-1, -1,8, 4, 4, 4,4,4,4,-1, -1, 12, 4, 4 0 
-edges: .word 0xf0000114, 0xf0000118, 0xf0000120, 0xf0000124, 0xf0000128,0xf0000164, 0xf0000168, 0xf000016c, 0xf00001a0 # is the last of row 2
+
+# -1 appears when we ignore or skip a led to be painted
+rowPathLen: .word 16, 12, 12, 12, 20, 12, 24, -1 # Row 1
+            .word 4,-1, -1,12,4,4,4,4,4,4, -1, -1, -1, 4, 4, 4, 4, -1, -1,4,-1 #Row 2
+            .word 4, 24, 12, 12, 4, 12, 4, 24, 0 # Row 3
+            
+# Edges are the literal borders of the maze or the inner walls to be ignored when painting            
+edges: .word 0xf0000114, 0xf0000118 # Row 1
+       .word 0xf0000120, 0xf0000124, 0xf0000134,0xf000013c, 0xf0000144,0xf000014c,0xf0000154, 0xf000015c, 0xf0000164,0xf0000168,0xf000016c,0xf0000174,0xf000017c, 0xf0000184, 0xf000018c, 0xf0000190, 0xf0000198, 0xf00001a0 # Row 2
+       .word 0xf00001a4,0xf00001ac, 0xf00001b0,0xf00001b4, 0xf000022c #Row 3
 
 .text
 la s1, LED_MATRIX_0_BASE
